f0db38c1da62146a0a62c9bad59bf8e3f6e204d9
bmouse1006 <bmouse1006@msn.com>
2012-06-01 21:50:20 +0800

    Project Initial

---
+++ PlaceAR/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PlaceAR
+//
+//  Created by 金 津 on 12-6-1.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
